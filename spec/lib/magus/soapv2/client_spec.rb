3135ba2c914e072f621cb7dd45e262f78fc1bfd0
Dave Nunez <david.nunez@berrettbrothers.com>
2013-07-30 15:30:32 -0700

    Write Initial Specs for SOAPv2 Client

---
+++ spec/lib/magus/soapv2/client_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+module Magento
+  module SOAPv2
+    describe Client, ".new" do
+      it "generates a login request" do
+        username = "jdoe"
+        api_key = "password"
+        Client.new(username, api_key)
+        expect(last_request).to match_valid_xml_body_for :login
+      end
+
+      it "sets @session_id to the session ID from the login request" do
+        pending "validate client#session_id against last_request session ID"
+      end
+    end
+  end
+end
